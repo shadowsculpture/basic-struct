6bd06299f6fbfb82e239daf701fe64cf7f5d7225
shadowsculptur1 <90444146+shadowsculpture@users.noreply.github.com>
2022-09-04 18:58:00 +0000

    Initial commit

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
-#include<stdio.h>
+language = bash;
+run = 
+/*#include<stdio.h>
 #include<stdlib.h>
 
 int max(int num1,int num2, int num3){
@@ -32,4 +34,36 @@ int main()
   
   return 0;
   
+}*/
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+
+  double num1;
+  double num2;
+  char op;
+
+  printf("Enter a number: ");
+  scanf("%f", &num1);
+  printf("Enter operator: ");
+  scanf("%lf", &num2);
+
+  if(op == '+'){
+    printf("%f",num1+num2);
+    
+  }else if(op == '-'){
+    printf("%f",num1-num2);
+    
+  }else if(op == '*'){
+    printf("%f",num1*num2);
+    }else if(op == '/'){
+    printf("%f",num1/num2);
+    
+    }else{
+    printf("Press any key to continue");
+  }
+  
+  return 0;
+
 }
